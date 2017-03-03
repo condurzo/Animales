a0cb8212a9163d7dfce12116a5e1ee6edb3e8b04
Maximiliano <maximiliano.condurzo@gmail.com>
2017-03-03 17:17:34 -0300

    Configuración Animaciones

--- Assets/Merengue/ManagerAnimaciones.cs
+++ Assets/Merengue/ManagerAnimaciones.cs
@@ -6,48 +6,41 @@ public class ManagerAnimaciones : MonoBehaviour {
 
 
 	public GameObject Gato;
+	public GameObject Perro;
 	public string currentAnimation;
+
+	void Start(){
+		Parado ();
+	}
 
 	public void Parado(){
 		if(currentAnimation != "Idle"){
-			//dog.GetComponent.<Animation>().Blend("Idle Dog");
-			Gato.GetComponent<Animation>().Blend("Idle Cat");
-			//turtle.GetComponent.<Animation>().Blend("Idle Turtle");
-			//owl.GetComponent.<Animation>().Blend("Idle Owl");
-			//rat.GetComponent.<Animation>().Blend("Idle Rat");
+			Gato.GetComponent<Animation>().Play("Idle Cat");
+			Perro.GetComponent<Animation>().Play("Idle Dog");
 			currentAnimation = "Idle";
 		}
 	}
 	public void Caminar(){
 		if(currentAnimation != "Walk"){
-			//dog.GetComponent.<Animation>().Blend("Walk Dog");
-			Gato.GetComponent<Animation>().Blend("Walk Cat");
-			//turtle.GetComponent.<Animation>().Blend("Walk Turtle");
-			//owl.GetComponent.<Animation>().Blend("Walk Owl");
-			//rat.GetComponent.<Animation>().Blend("Walk Rat");
+			Gato.GetComponent<Animation>().Play("Walk Cat");
+			Perro.GetComponent<Animation>().Play("Walk Dog");
 			currentAnimation = "Walk";
 		}
 	}
 	public void Correr(){
 		if(currentAnimation != "Run"){
-			//dog.GetComponent.<Animation>().Blend("Run Dog");
-			Gato.GetComponent<Animation>().Blend("Run Cat");
-			//turtle.GetComponent.<Animation>().Blend("Run Turtle");
-			//owl.GetComponent.<Animation>().Blend("Run Owl");
-			//rat.GetComponent.<Animation>().Blend("Run Rat");
+			Gato.GetComponent<Animation>().Play("Run Cat");
+			Perro.GetComponent<Animation>().Play("Run Dog");
 			currentAnimation = "Run";
 		}
 	}
 
 	public void Dormir(){
+		if(currentAnimation != "Sleep"){
+			Gato.GetComponent<Animation>().Play("Sleep Cat");
+			Perro.GetComponent<Animation>().Play("Sleep Dog");
+			currentAnimation = "Sleep";
+		}
 	}
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
--- Assets/Merengue/ManagerJump.js
+++ Assets/Merengue/ManagerJump.js
@@ -1,11 +1,32 @@
 ﻿#pragma strict
+#pragma downcast
 
-var cat : GameObject;
+
 private var currentAnimation : String;
 
-public function CatJump(){
+var cat : GameObject;
+ 
+
+
+
+
+public function Jump(){
 if(currentAnimation != "Jump"){
+		BlendAllCharactersToZero();
 		cat.GetComponent(JumpTest).enabled = true;
 		currentAnimation = "Jump";
+	}
+}
+
+function BlendAllCharactersToZero(){
+	BlendAllToZero(cat);
+
+	cat.GetComponent(JumpTest).enabled = false;
+}
+
+function BlendAllToZero(character : GameObject){
+	var animation = character.GetComponent.<Animation>();
+	for(var state : AnimationState in animation){
+		animation.Blend(state.name, 0);
 	}
 }
