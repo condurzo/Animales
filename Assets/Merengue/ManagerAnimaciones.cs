f60cc29ed3d53fe45ff09a127115b60b194e39d8
condurzo <maximiliano.condurzo@gmail.com>
2017-03-02 21:58:44 -0300

    Commit Inicial
    Commit Inicial

---
+++ Assets/Merengue/ManagerAnimaciones.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerAnimaciones : MonoBehaviour {
+
+
+	public GameObject Gato;
+	public string currentAnimation;
+
+	public void Parado(){
+		if(currentAnimation != "Idle"){
+			//dog.GetComponent.<Animation>().Blend("Idle Dog");
+			Gato.GetComponent<Animation>().Blend("Idle Cat");
+			//turtle.GetComponent.<Animation>().Blend("Idle Turtle");
+			//owl.GetComponent.<Animation>().Blend("Idle Owl");
+			//rat.GetComponent.<Animation>().Blend("Idle Rat");
+			currentAnimation = "Idle";
+		}
+	}
+	public void Caminar(){
+		if(currentAnimation != "Walk"){
+			//dog.GetComponent.<Animation>().Blend("Walk Dog");
+			Gato.GetComponent<Animation>().Blend("Walk Cat");
+			//turtle.GetComponent.<Animation>().Blend("Walk Turtle");
+			//owl.GetComponent.<Animation>().Blend("Walk Owl");
+			//rat.GetComponent.<Animation>().Blend("Walk Rat");
+			currentAnimation = "Walk";
+		}
+	}
+	public void Correr(){
+		if(currentAnimation != "Run"){
+			//dog.GetComponent.<Animation>().Blend("Run Dog");
+			Gato.GetComponent<Animation>().Blend("Run Cat");
+			//turtle.GetComponent.<Animation>().Blend("Run Turtle");
+			//owl.GetComponent.<Animation>().Blend("Run Owl");
+			//rat.GetComponent.<Animation>().Blend("Run Rat");
+			currentAnimation = "Run";
+		}
+	}
+
+	public void Dormir(){
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Merengue/ManagerJump.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var cat : GameObject;
+private var currentAnimation : String;
+
+public function CatJump(){
+if(currentAnimation != "Jump"){
+		cat.GetComponent(JumpTest).enabled = true;
+		currentAnimation = "Jump";
+	}
+}
